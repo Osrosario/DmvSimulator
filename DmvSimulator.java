1432b41cf80e9190dca996ed74fa6e99b980277a
Osrosario <osrosario1@gmail.com>
2023-03-07 11:57:15 -0500

    Added Miracle

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -7,17 +7,15 @@ class DmvSimulator
     public static void main(String[] args) throws InterruptedException
     {
         System.out.println("\nWelcome to the Worst Place on Earth! The DMV!");
-        Thread.sleep(5000);
         System.out.println("\nLet me give you a ticket that'll keep you here forever...");
-        Thread.sleep(5000);
 
         int randomNum = random.nextInt(200) + 1;
         
         System.out.println("Your number is " + randomNum + ". Wow! That sucks! We just called that number. Looks like you'll have to wait. I hope you brought something to read! Have a seat.\n");
-        Thread.sleep(5000);
 
         int calledNum = randomNum + 1;
         int windowNum = 1;
+        int miracleNum = random.nextInt(100) + 1;
         
         while (calledNum != randomNum)
         {
@@ -34,20 +32,22 @@ class DmvSimulator
             System.out.println("Number " + calledNum + ", please come to Window " + windowNum + ".");
             calledNum++;
             windowNum++;
-            Thread.sleep(1000);
         }
         
-        System.out.println("\nNumber " + calledNum + ", please come to Window " + windowNum + ".");
-        Thread.sleep(3000);
-        System.out.println("Amazing! You're still here! In that case, may I see your paperwork?");
-        Thread.sleep(5000);
-        System.out.println("\nYou hand in your paperwork...");
-        Thread.sleep(3000);
-        System.out.println("\nYou really waited all this time and don't even have the correct paperwork?? Shame...");
-        Thread.sleep(3000);
-        System.out.println("Looks like you'll have come back and get in line!");
-        Thread.sleep(3000);
-        System.out.println("MUHAHAHA! Your despair feeds our inefficient customer service! Begone!");
-        Thread.sleep(3000);
+        if (miracleNum == 27)
+        {
+            System.out.println("\nAmazing! You're still here! In that case, may I see your paperwork?");
+            System.out.println("\nYou hand in your paperwork...");
+            System.out.println("\nWow! You actually have the correct paperwork!");
+        }
+        else
+        {
+            System.out.println("\nNumber " + calledNum + ", please come to Window " + windowNum + ".");
+            System.out.println("Amazing! You're still here! In that case, may I see your paperwork?");
+            System.out.println("\nYou hand in your paperwork...");
+            System.out.println("\nYou really waited all this time and don't even have the correct paperwork?? Shame...");
+            System.out.println("Looks like you'll have come back and get in line!");
+            System.out.println("MUHAHAHA! Your despair feeds our inefficient customer service! Begone!");
+        }
     }
 }
